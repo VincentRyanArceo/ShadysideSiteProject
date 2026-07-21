dd5447acae04651b8d690c4ab8b4c47f3196c9b2
Vincent Ryan <vincent.ryan.dev@outlook.com>
2026-07-20 23:49:36 -0400

    add MusicController add Index.cshtml

---
+++ ShadysideSiteProject/Controllers/MusicController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ShadysideSiteProject.Controllers
+{
+    public class MusicController : Controller
+    {
+        // The Index action is the default method that runs when the cotroller is called
+        public IActionResult Index()
+        {
+            // This tells the app to find and display the HTML view
+            return View();
+        }
+    }
+}
